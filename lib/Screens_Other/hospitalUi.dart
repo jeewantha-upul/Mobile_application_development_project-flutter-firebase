cc0e2f520c1f3d9c7238c16cbfb453157d8b0e16
Janodh <69080518+Janodh@users.noreply.github.com>
2021-01-14 14:07:37 +0530

    Gmap Connect

--- lib/Screens_Other/hospitalUi.dart
+++ lib/Screens_Other/hospitalUi.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:mad/mainmap2.dart';
 
 class HospitalUi extends StatefulWidget {
   @override
@@ -12,12 +13,7 @@ class _HospitalUiState extends State<HospitalUi> {
       child: Expanded(
         child: GestureDetector(
           onTap: () {
-            /*--------------------------
-            -------------------------
-            add your code here
-            ---------------------------
-            -----------------------------
-             */
+            runApp(MyApp2());
           },
           child: Container(
             child: Column(
---
+++ lib/mainmap2.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
+import 'package:mad/models/place.dart';
+import 'package:mad/Screens_Other/search2.dart';
+import 'package:mad/services/geolocator_service.dart';
+import 'package:mad/services/places_service2.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:provider/provider.dart';
+
+
+
+class MyApp2 extends StatelessWidget {
+  final locatorService = GeoLocatorService();
+  final placesService = PlacesService();
+
+  @override
+  Widget build(BuildContext context) {
+    return MultiProvider(
+      providers: [
+        FutureProvider(create: (context) => locatorService.getLocation()),
+        FutureProvider(create: (context) {
+          ImageConfiguration configuration = createLocalImageConfiguration(context);
+          return BitmapDescriptor.fromAssetImage(configuration, 'image/bell.png');
+        }),
+        ProxyProvider2<Position,BitmapDescriptor,Future<List<Place>>>(
+          update: (context,position,icon,places){
+            return (position !=null) ? placesService.getPlaces(position.latitude, position.longitude,icon) :null;
+          },
+        )
+      ],
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'Medi clock',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        home: Search2(),
+      ),
+    );
+  }
+}
